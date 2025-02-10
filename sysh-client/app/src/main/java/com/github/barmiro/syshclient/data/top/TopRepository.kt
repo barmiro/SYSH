f7a23987d721f5bfd6df91ce44ae2e188a7db7b7
barmiro <barmiro1234@gmail.com>
2025-02-10 15:42:58 +0100

    getTop functions with Resource<T> and Flow integration, not tested

--- sysh-client/app/src/main/java/com/github/barmiro/syshclient/data/top/TopRepository.kt
+++ sysh-client/app/src/main/java/com/github/barmiro/syshclient/data/top/TopRepository.kt
@@ -1,7 +1,15 @@
 package com.github.barmiro.syshclient.data.top
 
+import com.github.barmiro.syshclient.data.top.dto.AlbumDTO
+import com.github.barmiro.syshclient.data.top.dto.ArtistDTO
+import com.github.barmiro.syshclient.data.top.dto.TrackDTO
+import com.github.barmiro.syshclient.util.Resource
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Inject
@@ -21,10 +29,104 @@ class TopRepository @Inject constructor() {
 
     val topApi = retrofit.create(TopApi::class.java)
 
-    suspend fun getTracks() {
+    suspend fun test() {
         println("Top track: " + topApi.fetchTopTracks("2024-01-01T00:00:00", "2024-12-31T23:59:59", "time").body().orEmpty()[0])
         println("Top album: " + topApi.fetchTopAlbums("2024-01-01T00:00:00", "2024-12-31T23:59:59", "time").body().orEmpty()[0])
         println("Top artist: " + topApi.fetchTopArtists("2024-01-01T00:00:00", "2024-12-31T23:59:59", "time").body().orEmpty()[0])
+    }
+
+//    I know this is bad, but refactoring will be made later on,
+//    when I know exactly which DTO contains what fields
+    suspend fun getTopTracks(
+        fetchFromRemote: Boolean,
+        start: String? = null,
+        end: String? = null,
+        sort: String? = null
+    ): Flow<Resource<List<TrackDTO>>> {
+        return flow {
+            emit(Resource.Loading(true))
+            val topTracks = try{
+                topApi.fetchTopTracks(start, end, sort)
+                    .body()
+                    .orEmpty()
+            } catch (e:IOException) {
+                e.printStackTrace()
+                emit(Resource.Error("Encountered IOException: " + e.message))
+                ArrayList()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error("Encountered HttpException: " + e.code()))
+                ArrayList()
+            }
+            val isFetchSuccessful = topTracks.isNotEmpty()
+            if (isFetchSuccessful) {
+                emit(Resource.Success(topTracks))
+            } else {
+                emit(Resource.Error("Received list is empty"))
+            }
+            emit(Resource.Loading(false))
+        }
+    }
+    suspend fun getTopAlbums(
+        fetchFromRemote: Boolean,
+        start: String? = null,
+        end: String? = null,
+        sort: String? = null
+    ): Flow<Resource<List<AlbumDTO>>> {
+        return flow {
+            emit(Resource.Loading(true))
+            val topAlbums = try{
+                topApi.fetchTopAlbums(start, end, sort)
+                    .body()
+                    .orEmpty()
+            } catch (e:IOException) {
+                e.printStackTrace()
+                emit(Resource.Error("Encountered IOException: " + e.message))
+                ArrayList()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error("Encountered HttpException: " + e.code()))
+                ArrayList()
+            }
+            val isFetchSuccessful = topAlbums.isNotEmpty()
+            if (isFetchSuccessful) {
+                emit(Resource.Success(topAlbums))
+            } else {
+                emit(Resource.Error("Received list is empty"))
+            }
+            emit(Resource.Loading(false))
+        }
+    }
+
+    suspend fun getTopArtists(
+        fetchFromRemote: Boolean,
+        start: String? = null,
+        end: String? = null,
+        sort: String? = null
+    ): Flow<Resource<List<ArtistDTO>>> {
+        return flow {
+            emit(Resource.Loading(true))
+            val topArtists = try{
+                topApi.fetchTopArtists(start, end, sort)
+                    .body()
+                    .orEmpty()
+            } catch (e:IOException) {
+                e.printStackTrace()
+                emit(Resource.Error("Encountered IOException: " + e.message))
+                ArrayList()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error("Encountered HttpException: " + e.code()))
+                ArrayList()
+            }
+            val isFetchSuccessful = topArtists.isNotEmpty()
+            if (isFetchSuccessful) {
+                emit(Resource.Success(topArtists))
+            } else {
+                emit(Resource.Error("Received list is empty"))
+            }
+            emit(Resource.Loading(false))
+        }
     }
 
 
---
+++ sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/top/TopTracksViewModel.kt
@@ -0,0 +1,4 @@
+package com.github.barmiro.syshclient.presentation.top
+
+class TopTracksViewModel {
+}
