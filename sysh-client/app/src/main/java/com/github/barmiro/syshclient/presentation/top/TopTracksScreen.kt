e3dc896560bd2e0a8f24d632317fcf7258a5476a
barmiro <barmiro1234@gmail.com>
2025-02-14 00:19:16 +0100

    added date range picker, not functional yet

--- sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/top/TopTracksScreen.kt
+++ sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/top/TopTracksScreen.kt
@@ -5,19 +5,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun TopTracksScreen(
@@ -25,6 +38,40 @@ fun TopTracksScreen(
 ) {
     val state = viewModel.state
     viewModel.observeLifecycle(LocalLifecycleOwner.current.lifecycle)
+
+    var dateRange by remember { mutableStateOf<Pair<Long?, Long?>?>(null) }
+
+    if(dateRange != null) {
+        val start = Date(dateRange!!.first!!)
+        val end = Date(dateRange!!.second!!)
+
+        val formattedStart = SimpleDateFormat(
+            "MMM dd, yyyy",
+            Locale.getDefault())
+            .format(start)
+
+        val formattedEnd = SimpleDateFormat(
+            "MMM dd, yyyy",
+            Locale.getDefault())
+            .format(end)
+
+        Text("Selected range: $formattedStart - $formattedEnd")
+    } else {
+        Text("No date range selected")
+    }
+
+    var isDateRangePickerVisible by remember { mutableStateOf(false) }
+
+    if (isDateRangePickerVisible) {
+        DateRangePickerModal(
+            onDateRangeSelected = {
+                dateRange = it
+            },
+            onDismiss = {
+                isDateRangePickerVisible = false
+            }
+        )
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -36,6 +83,13 @@ fun TopTracksScreen(
             }
         ) {
             Text(text = "Sort by time")
+        }
+        Button(
+            onClick = {
+                isDateRangePickerVisible = true
+            }
+        ) {
+            Text(text = "Select date range")
         }
         LazyColumn(
             modifier = Modifier.fillMaxSize()
@@ -70,5 +124,54 @@ fun <T: LifecycleObserver> T.observeLifecycle(lifecycle: Lifecycle) {
         onDispose {
             lifecycle.removeObserver(this@observeLifecycle)
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateRangePickerModal(
+    onDateRangeSelected: (Pair<Long?, Long?>) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val dateRangePickerState = rememberDateRangePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onDateRangeSelected(
+                        Pair(
+                            dateRangePickerState.selectedStartDateMillis,
+                            dateRangePickerState.selectedEndDateMillis
+                        )
+                    )
+                    onDismiss()
+                }
+            ) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onDismiss
+            ) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DateRangePicker(
+            state = dateRangePickerState,
+            title = {
+                Text(
+                    text = "Select date range"
+                )
+            },
+            showModeToggle = false,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(500.dp)
+                .padding(16.dp)
+        )
     }
 }
