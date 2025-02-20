7ac24fa4280bf04c8ddd2dea8c1af8cb6e36e86b
barmiro <barmiro1234@gmail.com>
2025-02-20 04:14:07 +0100

    disabled ripple on tab row, minor visual changes

--- sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/top/TopScreen.kt
+++ sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/top/TopScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DateRangePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabPosition
@@ -30,6 +31,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -39,6 +41,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,8 +55,11 @@ import com.github.barmiro.syshclient.presentation.top.tracks.TopTracksViewModel
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import kotlin.math.absoluteValue
 
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopScreen(
     viewModel: TopScreenViewModel,
@@ -129,13 +135,20 @@ fun TopScreen(
                         pagerState.getOffsetDistanceInPages(0))
                 }) {
                 tabs.forEachIndexed { index, title ->
-                    Tab(text = { Text(
-                        text = title,
-                        fontSize = 16.sp,
-                        color = MaterialTheme.colorScheme.onSecondaryContainer) },
-                        selected = tabIndex == index,
-                        onClick = { tabIndex = index }
-                    )
+                    CompositionLocalProvider(LocalRippleConfiguration provides null) {
+                        Tab(text = { Text(
+                            text = title,
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colorScheme.onSecondaryContainer,
+                            modifier = Modifier.alpha(
+                                (1 - pagerState.getOffsetDistanceInPages(index).absoluteValue / 2).coerceAtLeast(0.5f)
+                            )
+                        ) },
+                            selected = tabIndex == index,
+                            onClick = { tabIndex = index }
+                        )
+
+                    }
                 }
             }
 
