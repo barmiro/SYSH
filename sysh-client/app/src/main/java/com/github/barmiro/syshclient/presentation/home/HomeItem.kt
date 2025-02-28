251da07e00404ba11abc670422ec8bdd1124fdf4
barmiro <barmiro1234@gmail.com>
2025-02-28 23:22:58 +0100

    refactored home screen rows into HomeItem.kt

---
+++ sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/home/HomeItem.kt
@@ -0,0 +1,44 @@
+package com.github.barmiro.syshclient.presentation.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeItem(
+    itemText: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(5f).padding(horizontal = 8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = itemText,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.weight(5f)
+                )
+            }
+        }
+    }
+}
--- sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/home/HomeScreen.kt
+++ sysh-client/app/src/main/java/com/github/barmiro/syshclient/presentation/home/HomeScreen.kt
@@ -1,17 +1,13 @@
 package com.github.barmiro.syshclient.presentation.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(
@@ -23,54 +19,13 @@ fun HomeScreen(
     Column(
         modifier = Modifier.fillMaxSize().padding(32.dp),
     ) {
-        Row(
-        ) {
-            Text(text = "Welcome, " + state.userDisplayName,
-                fontSize = 32.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-        Row(
-        ) {
-            Text(text = "${state.stats.stream_count} streams",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-        Row(
-        ) {
-            Text(text = "${state.stats.minutes_streamed} minutes",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-        Row(
-        ) {
-            Text(text = "${state.stats.minutes_streamed / 60} hours",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-        Row(
-        ) {
-            Text(text = "${String.format("%.1f", state.stats.minutes_streamed / 1440f)} days",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-        Row(
-        ) {
-            Text(text = "${state.stats.track_count} tracks",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-        Row(
-        ) {
-            Text(text = "${state.stats.album_count} albums",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-        Row(
-        ) {
-            Text(text = "${state.stats.artist_count} artists",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onBackground)
-        }
-
+        HomeItem(itemText = "Welcome, " + state.userDisplayName)
+        HomeItem(itemText = "${state.stats.stream_count} streams")
+        HomeItem(itemText = "${state.stats.minutes_streamed} minutes")
+        HomeItem(itemText = "${state.stats.minutes_streamed / 60} hours")
+        HomeItem(itemText = "${String.format("%.1f", state.stats.minutes_streamed / 1440f)} days")
+        HomeItem(itemText = "${state.stats.track_count} tracks")
+        HomeItem(itemText = "${state.stats.album_count} albums")
+        HomeItem(itemText = "${state.stats.artist_count} artists")
     }
 }
