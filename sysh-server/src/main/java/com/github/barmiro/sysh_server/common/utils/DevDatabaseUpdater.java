67b219f5685ad2188a1b1e579a57ca61bd81985b
barmiro <barmiro1234@gmail.com>
2025-04-23 18:47:10 +0200

    database updater improvements

--- sysh-server/src/main/java/com/github/barmiro/sysh_server/common/utils/DevDatabaseUpdater.java
+++ sysh-server/src/main/java/com/github/barmiro/sysh_server/common/utils/DevDatabaseUpdater.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.core.simple.JdbcClient.StatementSpec;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,53 +37,87 @@ public class DevDatabaseUpdater {
 			updateLegacyDatabase();
 			
 			String currentVersion = getDBVersion();
-			System.out.println(currentVersion);
 			
 			if (!currentVersion.equals(TARGET_DATABASE_VERSION)) {
-				log.info("Updating database from version "
+				log.info(
+						"Updating database from v"
 						+ currentVersion
-						+ " to version "
+						+ " to v"
 						+ TARGET_DATABASE_VERSION
-						+ "...");
+						+ "..."
+				);
 				
-				updater("0.0.0",
-						"0.0.1",
-						"ALTER TABLE Users ADD COLUMN has_imported_data BOOLEAN DEFAULT false; "
-						+ "UPDATE Users SET timezone = 'UTC' WHERE timezone IS NULL; "
-						+ "ALTER TABLE Users ALTER COLUMN timezone SET NOT NULL;"
-						);
+				updater(
+					// expectedVersion
+					"0.0.0",
+					// targetVersion 
+					"0.0.1",
+					// updateSql
+					"ALTER TABLE Users "
+						+ "ADD COLUMN has_imported_data BOOLEAN DEFAULT false; "
+					+ "UPDATE Users "
+						+ "SET timezone = 'UTC' "
+						+ "WHERE timezone IS NULL; "
+					+ "ALTER TABLE Users "
+						+ "ALTER COLUMN timezone "
+						+ "SET NOT NULL;", 
+					// verificationSql
+					"SELECT has_imported_data FROM Users LIMIT 1;",
+					// verificationType
+					Boolean.class
+				);
 				
 				
 			} else {
-				log.info("Database is up to date (v"
+				log.info(
+						"Database is up to date (v"
 						+ currentVersion
-						+ ")");
+						+ ")"
+				);
 			}
 		}
 	}
 	
-	void updater(String expectedVersion,
+	/**
+	 * verificationSql has to be a query which returns exactly 1 result after a successful update,
+	 * and any other number of results (0, >1) before the update
+	 * @param <T> type of value to be returned by verificationSql
+	 */
+	<T> void updater(String expectedVersion,
 			String targetVersion,
-			String sql) {
+			String updateSql,
+			String verificationSql,
+			Class<T> verificationType) {
 		
 		String currentVersion = getDBVersion();
+		StatementSpec updateVersionNumber = jdbc.sql("UPDATE db_info SET version = :targetVersion WHERE id = true;")
+		.param("targetVersion", targetVersion, Types.VARCHAR);
 		
 		if (currentVersion.equals(expectedVersion)) {
-			int updated = jdbc.sql(sql
-					+ "UPDATE db_info SET version = :targetVersion WHERE id = true;")
-					.param("targetVersion", targetVersion, Types.VARCHAR)
-					.update();
-			if (updated > 0) {
-				log.info("Updated database from v"
-						+ currentVersion
-						+ " to v"
-						+ targetVersion);
+			
+			jdbc.sql(updateSql).update();
+			
+			int verification = jdbc.sql(verificationSql).query(verificationType).list().size();
+			
+			if (verification == 1) {
+				if (updateVersionNumber.update() == 1) {
+					log.info("Updated database from v"
+							+ currentVersion
+							+ " to v"
+							+ targetVersion);
+				} else {
+					throw new RuntimeException("Updating version number failed: from "
+							+ currentVersion
+							+ " to "
+							+ targetVersion);
+				}
 			} else {
-				log.error("Database update from version "
+				throw new RuntimeException("Database update from version "
 						+ currentVersion
 						+ " to version "
 						+ targetVersion
-						+ " failed");
+						+ " failed: "
+						+ verification);
 			}
 		}
 	}
@@ -95,15 +130,22 @@ public class DevDatabaseUpdater {
 	}
 	
 	private void updateLegacyDatabase() {
-		int updated = jdbc.sql("CREATE TABLE IF NOT EXISTS db_info ("
+		jdbc.sql("CREATE TABLE IF NOT EXISTS db_info ("
 					+ "id BOOLEAN PRIMARY KEY DEFAULT TRUE CHECK (id),"
-					+ "version VARCHAR NOT NULL);"
-					+ "INSERT INTO db_info (version) VALUES ('0.0.0') "
-					+ "ON CONFLICT (id) DO NOTHING;")
+					+ "version VARCHAR NOT NULL);")
+		.update();
+		
+		int updated = jdbc.sql("INSERT INTO db_info ("
+				+ "version"
+				+ ") VALUES ("
+				+ "'0.0.0'"
+				+ ") ON CONFLICT (id) DO NOTHING;")
 		.update();
 		
 		if (updated > 0) {
 			log.info("Updated database from legacy to numbered versioning system");
+		} else {
+			log.info("Legacy database updater returned " + updated);
 		}
 	}
 }
