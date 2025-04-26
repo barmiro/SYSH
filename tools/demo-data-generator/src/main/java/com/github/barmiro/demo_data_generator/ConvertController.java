90fc84deb22756402ed096ce4ed9007fede17feb
barmiro <barmiro1234@gmail.com>
2025-04-26 19:34:55 +0200

    implemented stream conversion logic

---
+++ tools/demo-data-generator/src/main/java/com/github/barmiro/demo_data_generator/ConvertController.java
@@ -0,0 +1,52 @@
+package com.github.barmiro.demo_data_generator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.barmiro.demo_data_generator.dto.StreamDTO;
+
+@RestController
+public class ConvertController {
+
+	
+	List<StreamDTO> rawStreams = new ArrayList<>();
+	Map<String, String> idSwitchMap = new HashMap<>();
+	
+	@PostMapping("/json")
+	int jsonToList(@RequestBody List<StreamDTO> streams) {
+		rawStreams.addAll(streams);
+		return rawStreams.size();
+	}
+	
+	@PostMapping("/convert")
+	List<StreamDTO> convertStreams(@RequestBody List<String> newIDs) {
+		int index = 0;
+		List<StreamDTO> convertedStreams = new ArrayList<>();
+		for (StreamDTO rawStream:rawStreams) {
+			if (!idSwitchMap.containsKey(rawStream.spotify_track_uri()) && index < newIDs.size()) {
+				idSwitchMap.put(rawStream.spotify_track_uri(), "spotify:track:" + newIDs.get(index));
+				index++;
+			}
+		}
+		for (StreamDTO rawStream:rawStreams) {
+			convertedStreams.add(
+					new StreamDTO(
+							rawStream.ts(),
+							rawStream.ms_played(),
+							idSwitchMap.get(rawStream.spotify_track_uri())
+							)
+					);
+		}
+		
+		System.out.println(index);
+		return convertedStreams;
+	}
+	
+	
+}
